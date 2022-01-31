f746fd593ec52b42b0e99253361b8c2dfa307048
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-01-31 11:50:46 +0530

    code added

---
+++ recursion/subsetsum.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+
+public class subsetsum {
+    
+    public static void main(String[] args) {
+        int[][] arr= {{1,2,3},{3,2,1}};
+        System.out.println(maximumWealth(arr));
+        
+    }
+
+    static int subset(int[] arr)
+    {
+      
+        int sum=0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        sum=2*sum;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                sum+=arr[i]+arr[j];
+            }
+        }
+        return sum;
+    }
+    static void subsetsumprint(int[] arr,int idx,int sum,ArrayList<Integer> ans)
+    {
+        if(idx==arr.length)
+        {
+            ans.add(sum);
+            if(sum==0)
+            
+            System.out.println(ans);
+            
+            return;
+        }
+
+
+        sum+=arr[idx];
+        subsetsumprint(arr, idx+1, sum, ans);
+        sum-=arr[idx];
+         subsetsumprint(arr, idx+1, sum, ans);
+       
+
+    }
+      static int maximumWealth(int[][] arr) {
+        
+      
+        int maxsum=0;
+        
+        for(int i=0; i<arr.length; i++)
+        {  int sum=0;
+            for(int j=0; j<arr[0].length; j++)
+            {
+                sum = sum + arr[i][j];
+                if(maxsum<sum)
+                {
+                    maxsum=sum;
+                }
+            }
+        }
+        return maxsum;
+    }
+}
