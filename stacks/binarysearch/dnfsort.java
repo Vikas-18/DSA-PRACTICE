10388d2be66bff3c6eaba493a1ee417d1eec1120
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-03-09 10:59:53 +0530

    files added

---
+++ stacks/binarysearch/dnfsort.java
@@ -0,0 +1,62 @@
+package binarysearch;
+import java.util.*;
+public class dnfsort {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(0);
+        a.add(1);
+        a.add(2);
+        a.add(0);
+        a.add(1);
+        a.add(2);
+
+        sortColors(a);
+    }
+    static void sortColors(ArrayList<Integer> a) {
+        if(a.size()==1)
+        {
+            return;
+        }
+        int ones=0;
+        int twos=0;
+        int zeros=0;
+
+        for(int i=0; i<a.size(); i++)
+        {
+            if(a.get(i)==0)
+            {
+                zeros++;
+            }
+
+            if(a.get(i)==1)
+            {
+                ones++;
+            }
+
+            if(a.get(i)==2)
+            {
+                twos++;
+            }
+        }
+a.clear();
+        while(zeros>0 || ones>0 || twos>0)
+        {
+             if(zeros!=0)
+             {
+                 a.add(0);
+                 zeros--;
+             }
+
+             else if(ones!=0)
+             {
+                 a.add(1);
+                 ones--;
+             }
+
+             else{
+                 a.add(2);
+                 twos--;
+             }
+        }
+	}
+}
---
+++ stacks/binarysearch/peakfinding.java
@@ -0,0 +1,34 @@
+package binarysearch;
+import java.util.*;
+public class peakfinding {
+    public static void main(String[] args) {
+       
+    }
+
+    static  int perfectPeak(int[] A) {
+        
+        int s=0;
+        int e = A.length-1;
+
+        while(s<e)
+        {
+            int mid = s+(e-s)/2;
+
+            if(A[mid]<A[mid+1] && A[mid]>A[mid-1])
+            {
+                return mid;
+            }
+
+            else if(A[mid]<A[mid+1])
+            {
+                s= mid+1;
+            }
+
+            else{
+                e = mid-1;
+            }
+        }
+
+        return -1;
+    }
+}
