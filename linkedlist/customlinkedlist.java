61dc043e736a00073c2f75770f9ce0350e06b679
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-03-19 21:38:56 +0530

    alogoritms added

--- linkedlist/customlinkedlist.java
+++ linkedlist/customlinkedlist.java
@@ -1,31 +1,132 @@
 public class customlinkedlist {
     public static void main(String[] args) {
+        customlinkedlist ll = new customlinkedlist();
+        ll.insert(13);
+        ll.insert(11);
+        ll.insert(15);
+        ll.insert(18);
+        ll.size();
+        ll.print();
+        ll.reverse();
         
     }
-}
 
-class linkedlist{
+    private Node head;
+    private int size =0;
     class Node{
-        int data;
-        Node next;
+      private  int data;
+       private Node next;
+
+         Node(int data)
+       {
+        this.data = data;
+        this.next = null;
+        
+       }
     }
 
-    linkedlist(int data,int next)
-    {
-        this.data = data;
-        this.next = next;
-    }
-    linkedlist(int data)
-    {
-        this.data = data;
-        
-    }
+ 
 
     public void insert(int data)
     { 
-        if()    
-        linkedlist node = new linkedlist(data);
-        node.next = null;
+       Node node = new Node(data);
+        if(head==null)
+        {
+            head = node;
+            size++;
+            return;
+        }    
+      
+        node.next = head;
+        head = node;
+        size++;
     }
+
+    public void print()
+    {
+        Node temp = head;
+        
+        while(temp!=null)
+        {
+            System.out.print(temp.data+"->");
+            
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+//reverse a linked list
+    public void reverse()
+    {
+        Node curr = head;
+        Node prev = null;
+        Node next = null;
+
+        while(curr!=null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node temp = prev;
+        
+        while(temp!=null)
+        {
+          System.out.print(temp.data+"->");
+          
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+//adjacent swap nodes
+     public Node swapPairs(Node head) {
+        if(head==null)
+        {
+            return head;
+        }
+        int k=0;
+        Node curr = head;
+        Node prev= null;
+        Node next = null;
+        
+        while(curr!=null && k<2)
+        {
+           next = curr.next;
+           curr.next = prev;
+            prev = curr;
+            curr = next;
+            k++;
+        }
+        
+        if(next!=null)
+        {
+            head.next = swapPairs(next);
+            
+            
+        }
+        
+        return prev;
+
+    }
+
+    public void size()
+    {
+        Node temp = head;
+        int count = 0;
+        while (temp!=null) {
+            
+            count++;
+            temp = temp.next;
+        }
+
+        System.out.println(count);
+       
+      
+    }
+
+    
+
 }
+
 
