4bb0a12a4bd70c21a790b81aee3ebc568685bdf3
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-03-23 21:17:34 +0530

    leetcode problems added

--- linkedlist/customlinkedlist.java
+++ linkedlist/customlinkedlist.java
@@ -1,13 +1,7 @@
+
 public class customlinkedlist {
     public static void main(String[] args) {
-        customlinkedlist ll = new customlinkedlist();
-        ll.insert(13);
-        ll.insert(11);
-        ll.insert(15);
-        ll.insert(18);
-        ll.size();
-        ll.print();
-        ll.reverse();
+        customlinkedlist ll1 = new customlinkedlist();
         
     }
 
@@ -124,9 +118,114 @@ public class customlinkedlist {
        
       
     }
-
+//add two numbers 2
+public Node addTwoNumbers(Node l1, Node l2) {
+        Node ll1 = reverse(l1);
+        Node ll2 = reverse(l2);
+        
+        Node ans = new Node(-1);
+        Node dummy = ans;
+        
+        int carry = 0;
+        while(ll1!=null && ll2!=null)
+        {
+            int sum = ll1.data + ll2 .data + carry;
+            int lastdigit = sum%10;
+            dummy.next = new Node(lastdigit);
+            carry = sum/10;
+            ll1 = ll1.next;
+            ll2 = ll2.next;
+            dummy = dummy.next;
+        }
+        
+         while(ll1!=null)
+        {
+            int sum = ll1.data  + carry;
+            int lastdigit = sum%10;
+            dummy.next = new Node(lastdigit);
+            carry = sum/10;
+              ll1 = ll1.next;
+              dummy = dummy.next;
+        }
+        
+            while(ll2!=null)
+        {
+            int sum = ll2.data  + carry;
+            int lastdigit = sum%10;
+            dummy.next = new Node(lastdigit);
+            carry = sum/10;
+                 ll2 = ll2.next;
+                 dummy = dummy.next;
+        }
+            while(carry!=0)
+        {
+            int sum = carry;
+            int lastdigit = sum%10;
+            dummy.next = new Node(lastdigit);
+            carry = sum/10;
+            dummy = dummy.next;
+        }
+        
+      
+        return ans.next;
+    }
     
+    public Node reverse(Node head)
+    {
+       Node prev = null;
+       Node next = null;
+        Node curr = head;
+        
+        while(curr!=null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev =curr;
+            curr =next;
+        }
+        return prev;
+    }
+    
+      public Node reorderList(Node head) {
+         if(head==null || head.next==null)
+        {
+            return head;
+        }
+        Node ans = head;
+        Node mid = middle(head);
+        Node hs = reverse(mid);
+        Node hf = head;
+        
+        while(hf!=null && hs!=null)
+        {
+            Node temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+            
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+            
+        }
+        if(hf!=null)
+        {
+            hf.next =null;
+        }
+        return ans;
 
+    }
+    public Node middle(Node head)
+    {
+        Node f = head;
+        Node s = head;
+        
+        while(f!=null && f.next!=null)
+        {
+            f = f.next.next;
+            s = s.next;
+        }
+        return s;
+    }
 }
 
 
