9251cae9f1228b969d3eeaa6c0adc36c792fa884
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-04-16 15:05:28 +0530

    binarysearch2darr

--- arrays/search2d.java
+++ arrays/search2d.java
@@ -1,7 +1,7 @@
 public class search2d {
     public static void main(String[] args) {
         int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
-        System.out.println(searchMatrix(arr, 10));
+        System.out.println(binarysearchMatrix(arr, 5));
     }
    static boolean searchMatrix(int[][] matrix, int target) {
         int r = 0;
@@ -20,6 +20,50 @@ public class search2d {
             }
             else{
                 c--;
+            }
+        }
+        return false;
+    }
+    static boolean binarysearchMatrix(int[][] matrix, int target) {
+        
+        int l=0;
+        int h= matrix.length-1;
+        
+        while(l<=h)
+        {
+           int mid = l+(h-l)/2;
+ if(matrix[mid][0]<=target && matrix[mid][matrix[0].length-1]>=target)
+            {
+                //binary search
+                int s=0;
+                int e = matrix[0].length-1;
+               
+                while(s<=e)
+                {
+                   int middle = s+(e-s)/2;
+                    if(matrix[mid][middle]==target)
+                    {
+                        return true;
+                    }
+                    
+                    else if(matrix[mid][middle]>target)
+                    {
+                        e = middle-1;
+                    }
+                    
+                    else{
+                        s = middle+1;
+                    }
+                }
+               return false;
+            }
+            
+else if(matrix[mid][0]<=target && matrix[mid][matrix[0].length-1]<=target)
+            {
+                l = mid+1;
+            }
+            else{
+                h = mid-1;
             }
         }
         return false;
