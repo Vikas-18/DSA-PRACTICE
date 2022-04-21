ce7c98a3b3fe94076604aecc1741dbe50538e9be
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-04-21 07:50:51 +0530

    productexceptitself_que_added

---
+++ arrays/productofarray.java
@@ -0,0 +1,31 @@
+public class productofarray {
+    public static void main(String[] args) {
+        
+    }
+
+   static int[] productExceptSelf(int[] nums) {
+        //right array banao which contains product afterwards
+        int[] right = new int[nums.length];
+        int mult = 1;
+        for(int i = nums.length-1; i>=0; i--)
+        {
+           mult*=nums[i];
+            right[i] = mult;
+        }
+        //left array jaise kaam karega contains product from start till that element
+        mult = 1;
+        int[] ans = new int[nums.length];
+        
+        for(int i=0; i<nums.length-1; i++)
+        {
+        
+            int rightproduct = right[i+1];
+            
+            ans[i] = mult*rightproduct;
+            mult*=nums[i];
+        }
+        ans[ans.length-1] = mult;
+        return ans;
+    }
+
+}
