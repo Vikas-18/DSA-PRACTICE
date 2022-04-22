ea7fdc360d50af499a38c599d9e45edc5ce11545
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-04-22 17:44:45 +0530

    que added

--- arrays/spiral2matrix.java
+++ arrays/spiral2matrix.java
@@ -1,6 +1,6 @@
 public class spiral2matrix {
     public static void main(String[] args) {
-        int[][] ans = generateMatrix(20);
+        int[][] ans = generateMatrix(3);
         for (int i = 0; i < ans.length; i++) {
             for (int j = 0; j < ans[0].length; j++) {
                 System.out.print(ans[i][j] + " ");
