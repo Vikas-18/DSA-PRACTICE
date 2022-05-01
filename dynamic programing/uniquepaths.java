dc04555916e950ec98763a211d1f35a241b43622
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-05-01 21:18:03 +0530

    uniquepath_swith_obstacles

--- dynamic programing/uniquepaths.java
+++ dynamic programing/uniquepaths.java
@@ -1,7 +1,14 @@
 public class uniquepaths {
     public static void main(String[] args) {
-        boolean[][] arr = new boolean[3][7];
-        System.out.println(solve(arr, 0, 0));
+        int[][] arr = {{1}};
+        boolean[][] isvisited = new boolean[1][1];
+        int[][] dp = new int[arr.length][arr[0].length];
+        for (int index = 0; index < arr.length; index++) {
+            for (int i = 0; i < arr[0].length; i++) {
+                dp[index][i] = -1;
+            }
+        }
+        System.out.println(solve2(arr,isvisited,0,0,dp));
     }
     static int ans1 = 0;
     static int ans2 = 0;
@@ -19,5 +26,36 @@ public class uniquepaths {
         ans1 = solve(arr, row+1, col);
         ans2 = solve(arr, row, col+1);
        return ans1 + ans2;
+    }
+
+    public static int solve2(int[][]arr,boolean[][]isvisited,int row,int col,int[][]dp)
+    {
+        if(arr[row][col]!=1 && row==0 && col==0)
+        {
+            return 1;
+        }
+        
+        if(row<0 || col<0)
+        {
+            return 0;
+        }
+        
+        if(dp[0][0]!=-1)
+        {
+            return dp[row][col];
+        }
+        
+         if(arr[row][col]!=1 && isvisited[row][col]==false)
+        {
+          isvisited[row][col] = true;  
+          int up = solve2(arr,isvisited,row-1,col,dp);
+          isvisited[row][col] = false; 
+          isvisited[row][col] = true; 
+          int left = solve2(arr,isvisited,row,col-1,dp);
+          isvisited[row][col] = false;
+             dp[row][col] = up+left;
+            return up + left;
+        }
+        return 0;
     }
 }
