96bdb3a8ab91569698fb4938d90584dec9d55409
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-06-29 12:46:20 +0530

    changes made in code

--- recursion/combination_nCr.java
+++ recursion/combination_nCr.java
@@ -1,24 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
-public class combination_nCr {
+public class Combination_nCr {
     public static void main(String[] args) {
         System.out.println(solve(5,2));
-        ArrayList<Integer> res = new ArrayList<>();
-        res.add(3);
-        res.add(5);
-        res.add(6);
-        res.add(9);
-        int[] ans = new int[res.size()];
-        
-        for(int i=0; i<ans.length; i++)
-        {
-            ans[0] = res.get(i);
-        }
-
-        System.out.println(Arrays.toString(ans));
-        
-     
     }
 
     static int solve(int n,int r)
