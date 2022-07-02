da995b54a01ae8cae60779dacf26ddc1b2e5e9c8
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-07-02 09:51:10 +0530

    changes made

--- dynamic programing/jump_games2.java
+++ dynamic programing/jump_games2.java
@@ -1,29 +1,37 @@
+import java.util.Arrays;
+
 public class jump_games2 {
     public static void main(String[] args) {
-        int[] arr = {2,3,1,1,4};
-        System.out.println(ans+1);
+        int[] nums = {2,1};
+        int[]dp = new int[nums.length+1];
+        Arrays.fill(dp, -1);
+        System.out.println(solve(nums, 0,dp));
+      
     }
 
 //[2,3,1,1,4]
-    public static int ans = Integer.MAX_VALUE;
-    public static void solve(int[]arr, int idx,int counter)
+public static int solve(int[]arr,int idx,int[]dp)
+{
+    //base case
+    if(idx==arr.length-1)
     {
-           //base case
-           if(idx+arr[idx]==arr.length-1)
-           {
-             ans = Math.min(ans, counter);
-             return;
-
-           }
-
-
-
-           for (int i = 1; i < arr[idx]; i++) {
-                counter+=1;
-                solve(arr, idx+i, counter);
-                counter-=1;
-           }
-
-
+      return 0;
     }
+    if(idx>=arr.length)
+    {
+        return Integer.MAX_VALUE;
+    }
+    if(dp[idx]!=-1)
+    {
+        return dp[idx];
+    }
+    int mini = 10000;
+    for(int i=1; i<=arr[idx]; i++)
+    {
+      
+        mini = Math.min(mini,solve(arr, idx+i,dp));
+    }
+    return dp[idx] =  mini+1;
 }
+
+ }
