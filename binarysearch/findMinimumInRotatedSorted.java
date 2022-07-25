5574231366243b4e687f03f894b6685c9130e945
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-07-25 09:57:54 +0530

    que added

---
+++ binarysearch/findMinimumInRotatedSorted.java
@@ -0,0 +1,29 @@
+public class findMinimumInRotatedSorted {
+    public static void main(String[] args) {
+        int[] nums = {4,6,7,8,9,1,2};
+        System.out.println(findMin(nums));
+    }
+
+    public static int findMin(int[] nums) {
+        int s = 0;
+        int e = nums.length-1;
+        if(nums[0]<=nums[nums.length-1])
+            return nums[0];
+        while(s<=e)
+        {
+            int mid = s + (e-s)/2;
+     
+            if(nums[mid]<=nums[(mid+nums.length-1)%nums.length])
+            {
+                return nums[mid];
+            }
+            
+            else if(nums[mid] >= nums[0]) 
+                s = mid+1;
+            else 
+                e = mid-1;
+        }
+        
+        return 0;
+    }
+}
