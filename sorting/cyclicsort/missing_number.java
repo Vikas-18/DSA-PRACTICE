65da00d29a75d05c4bb93ba0c729eccd9bad1b17
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-08-13 08:40:45 +0530

    new code added

--- sorting/cyclicsort/missing_number.java
+++ sorting/cyclicsort/missing_number.java
@@ -2,8 +2,8 @@ package cyclicsort;
 
 public class missing_number {
     public static void main(String[] args) {
-        int[] nums = {9,6,4,2,3,5,7,0,1};
-        System.out.println(missingNumber(nums));
+        int[] arr = {1};
+        System.out.println(MissingNumber(arr, 1));
     }
     
      static int missingNumber(int[] nums) {
@@ -38,4 +38,36 @@ public class missing_number {
         nums[x] = nums[y];
         nums[y] = temp;
     }
+
+    public static int MissingNumber(int arr[], int n) {
+        int i=0;
+        while(i<arr.length)
+        {
+            int correctindex = arr[i]-1;
+            
+            if(arr[i]<=arr.length && arr[i]!=arr[correctindex])
+            {
+                swap(arr,i,correctindex);
+            }
+            else{
+                i++;
+            }
+        }
+        
+        for(i=0; i<arr.length; i++)
+        {
+            if(arr[i]!=i+1)
+            {
+                return i+1;
+            }
+        }
+        return arr.length+1;
+     }
+     
+     public static void swap(int[]arr,int x,int y)
+     {
+         int temp = arr[x];
+         arr[x] = arr[y];
+         arr[y] = temp;
+     }
 }
