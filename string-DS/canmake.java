a7728613235ccb5e7f2fa35b92e58d58083ad263
Vikas-18 <vikaskohli18impossible@gmail.com>
2022-08-25 20:35:12 +0530

    que added

---
+++ string-DS/canmake.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class canmake {
+    public static void main(String[] args) {
+        System.out.println(canConstruct("aab", "baa"));
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<magazine.length(); i++)
+        {
+            if(map.containsKey(magazine.charAt(i)))
+            {
+                map.put(magazine.charAt(i),map.get(magazine.charAt(i))+1);
+            }
+            else{
+                 map.put(magazine.charAt(i),1);
+            }
+        }
+        
+         
+         for(int i=0; i<ransomNote.length(); i++)
+        {
+           if(map.containsKey(ransomNote.charAt(i)) && map.get(ransomNote.charAt(i))>0)
+           {
+                map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i))-1);
+           }
+             else{
+                 return false;
+             }
+        }
+        return true;
+        
+    }
+}
