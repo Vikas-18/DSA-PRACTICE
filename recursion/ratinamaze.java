62b80f0b6bb8d7cf9bcc9093969be186dd211e7e
Vikas-18 <vikaskohli18impossible@gmail.com>
2023-03-31 16:35:29 +0530

    que added

--- recursion/ratinamaze.java
+++ recursion/ratinamaze.java
@@ -7,17 +7,113 @@ public class ratinamaze {
 // {1,1,1,0},
 // {1,1,1,1}}
     public static void main(String[] args) {
-        int[][] maze={{1,1,0,1},
-        {1,1,1,0},
-        {1,1,1,1}};
-        ArrayList<String> list = new ArrayList<>();
-        ArrayList<ArrayList<String>> ans = new ArrayList<>();
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
-        ways(maze, 0, 0,list,ans,visited);
+        int[][] maze={{1,-1,1},
+                      {1,1,1},
+                      {1,1,1}};
+        boolean[][] vis = new boolean[maze.length][maze[0].length];
+        System.out.print(waysCount(maze, 0, 0, vis));
+
+        // ArrayList<String> list = new ArrayList<>();
+        // ArrayList<ArrayList<String>> ans = new ArrayList<>();
+        // boolean[][] visited = new boolean[maze.length][maze[0].length];
+        // ways(maze, 0, 0,list,ans,visited);
        
-        System.out.println(ans);
+        // System.out.println(ans);
 
 
+    }
+    // up down left right
+    public static int waysCount(int[][]arr,int r,int c,boolean[][]vis)
+    {
+        if(r==arr.length-1 && c==arr.length-1)
+        {
+            return 1;
+        }
+        if(r<0 || r>=arr.length || c<0 || c>=arr[0].length)
+        {
+            return 0;
+        }
+
+        int up = 0;
+        if(vis[r][c]==false && arr[r][c]==1)
+        {
+            vis[r][c] = true;
+           up = waysCount(arr, r-1, c,vis);
+           vis[r][c] = false;
+        }
+        int down = 0;
+        if(vis[r][c]==false && arr[r][c]==1)
+        {
+            vis[r][c] = true;
+          down = waysCount(arr, r+1, c,vis);
+          vis[r][c] = false;
+        }
+        int left = 0;
+        if(vis[r][c]==false && arr[r][c]==1)
+        {
+            vis[r][c] = true;
+           left = waysCount(arr, r, c-1,vis);
+           vis[r][c] = false;
+        }
+        int right = 0;
+        if(vis[r][c]==false && arr[r][c]==1)
+        {
+            vis[r][c] = true;
+           right= waysCount(arr, r, c+1,vis);
+           vis[r][c] = false;
+        }
+
+        return up+down+left+right;
+    }
+
+    static void printPath(int[][]arr,int r,int c,boolean[][]vis,ArrayList<String> ans)
+    {
+        if(r==arr.length-1 && c==arr[0].length-1)
+        {
+            System.out.println(ans);
+            return;
+        }
+        if(r<0 || r>=arr.length || c<0 || c>=arr[0].length)
+        {
+            return;
+        }
+        
+        if(vis[r][c]==false)
+        {
+           vis[r][c] = true;
+           ans.add("U");
+           waysCount(arr, r-1, c,vis);
+           ans.remove(ans.size()-1);
+           vis[r][c] = false;
+        }
+        if(vis[r][c]==false)
+        {
+            vis[r][c] = true;
+            ans.add("D");
+           waysCount(arr, r+1, c,vis);
+           ans.remove(ans.size()-1);
+            vis[r][c] = false;
+        }
+   
+        if(vis[r][c]==false)
+        {
+            vis[r][c] = true;
+            ans.add("L");
+           waysCount(arr, r, c-1,vis);
+           ans.remove(ans.size()-1);
+            vis[r][c] = false;
+        }
+      
+        if(vis[r][c]==false)
+        {
+           vis[r][c] = true;
+           ans.add("R");
+           waysCount(arr, r, c+1,vis);
+           ans.remove(ans.size()-1);
+           vis[r][c] = false;
+        }
+
+        
     }
 
     static void ways(int[][] maze,int n,int m ,ArrayList<String> list,ArrayList<ArrayList<String>> ans,boolean[][] visited)
