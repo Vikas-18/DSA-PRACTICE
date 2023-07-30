5a37faf637a3bed5a0738c30cf2fbb351560df4b
Vikas-18 <vikaskohli18impossible@gmail.com>
2023-07-30 08:55:04 +0530

    changes made in code

--- GraphTheory/topologicalSort.java
+++ GraphTheory/topologicalSort.java
@@ -2,43 +2,43 @@
 public class topologicalSort {
 
     // public static void main(String[] args) {
-        
+
     // }
-    // public static void dfs(int start,int[]vis,ArrayList<ArrayList<Integer>> adj,Stack<Integer> st)
+    // public static void dfs(int start,int[]vis,ArrayList<ArrayList<Integer>>
+    // adj,Stack<Integer> st)
     // {
-    //     vis[start] = 1;
-        
-        
-    //     for(Integer i: adj.get(start))
-    //     {
-    //         if(vis[i]==0)
-    //         {
-    //             dfs(i,vis,adj,st);
-    //         }
-    //     }
-    //     st.add(start);// after function call is over we add it to the stack
+    // vis[start] = 1;
+
+    // for(Integer i: adj.get(start))
+    // {
+    // if(vis[i]==0)
+    // {
+    // dfs(i,vis,adj,st);
     // }
-    // //Function to return list containing vertices in Topological order. 
-    // static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    // }
+    // st.add(start);// after function call is over we add it to the stack
+    // }
+    // //Function to return list containing vertices in Topological order.
+    // static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj)
     // {
-    //     int[]vis = new int[V];
-    //     Stack<Integer> st = new Stack<>();
-        
-    //     for(int i=0; i<V; i++)
-    //     {
-    //         if(vis[i]==0)
-    //         {
-    //             dfs(i,vis,adj,st);
-    //         }
-    //     }
-        
-    //     int[]ans = new int[V];
-    //     int idx=0;
-    //     while(st.size()>0)
-    //     {
-    //         ans[idx++] = st.peek();
-    //         st.pop();
-    //     }
-    //     return ans;
+    // int[]vis = new int[V];
+    // Stack<Integer> st = new Stack<>();
+
+    // for(int i=0; i<V; i++)
+    // {
+    // if(vis[i]==0)
+    // {
+    // dfs(i,vis,adj,st);
+    // }
+    // }
+
+    // int[]ans = new int[V];
+    // int idx=0;
+    // while(st.size()>0)
+    // {
+    // ans[idx++] = st.peek();
+    // st.pop();
+    // }
+    // return ans;
     // }
 }
--- binarytrees/buildtree.java
+++ binarytrees/buildtree.java
@@ -3,53 +3,47 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
-
+import java.util.Scanner;
 
 public class buildtree {
-    public static class Node
-    {
+    public static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data)
-        {
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
-        static int idx =-1;
-        public static Node buildTree(int[] arr)
-        {
-            idx++;
-            if(arr[idx]==-1)
-            {
-                return null;
-            }
 
-            Node newNode = new Node(arr[idx]);
-            newNode.left = buildTree(arr);
-            newNode.right = buildTree(arr);
+    static int idx = -1;
 
-            return newNode;
+    public static Node buildTree(int[] arr) {
+        idx++;
+        if (arr[idx] == -1) {
+            return null;
         }
-    
-    public static void preOrder(Node root)
-    {
-        if(root==null)
-        {
+
+        Node newNode = new Node(arr[idx]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+
+        return newNode;
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) {
             return;
         }
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
-    public static void levelOrder(Node root)
-    {
-        if(root==null)
-        {
+    public static void levelOrder(Node root) {
+        if (root == null) {
             return;
         }
         Queue<Node> q = new LinkedList<>();
@@ -58,108 +52,95 @@ public class buildtree {
 
         while (!q.isEmpty()) {
             Node currNode = q.remove();
-            if(currNode==null)
-            {
+            if (currNode == null) {
                 System.out.println();
                 // if(q.isEmpty())
                 // {
-                //     break;
+                // break;
                 // }
                 // else{
-                //     q.add(null);
+                // q.add(null);
                 // }
-            }
-            else{
+            } else {
                 System.out.print(currNode.data);
-                if(currNode.left!=null)
-                {
-                   q.add(currNode.left);
+                if (currNode.left != null) {
+                    q.add(currNode.left);
                 }
-                if(currNode.right!=null)
-                {
+                if (currNode.right != null) {
                     q.add(currNode.right);
                 }
             }
         }
     }
-    public static class Info
-    {
+
+    public static class Info {
         Node node;
         int hd;
-        public Info(Node node,int hd)
-        {
+
+        public Info(Node node, int hd) {
             this.node = node;
             this.hd = hd;
         }
     }
-    public static void topView(Node root)
-    {
-        //store horizantal distance and print only those which occurs for the first time
+
+    public static void topView(Node root) {
+        // store horizantal distance and print only those which occurs for the first
+        // time
         Queue<Info> q = new LinkedList<>();
-        HashMap<Integer,Node> map = new HashMap<>();
-        q.add(new Info(root,0));
-        int min = 0,max=0;
+        HashMap<Integer, Node> map = new HashMap<>();
+        q.add(new Info(root, 0));
+        int min = 0, max = 0;
 
-
-        while(!q.isEmpty())
-        {
+        while (!q.isEmpty()) {
             Info currNode = q.remove();
 
-            if(!map.containsKey(currNode.hd))
-            {
+            if (!map.containsKey(currNode.hd)) {
                 map.put(currNode.hd, currNode.node);
             }
 
-            if(currNode.node.left!=null)
-            {
-                q.add(new Info(currNode.node.left,currNode.hd-1));
-                min = Math.min(min,currNode.hd-1);
+            if (currNode.node.left != null) {
+                q.add(new Info(currNode.node.left, currNode.hd - 1));
+                min = Math.min(min, currNode.hd - 1);
             }
-            if(currNode.node.right!=null)
-            {
-               q.add(new Info(currNode.node.right,currNode.hd+1));
-                max = Math.max(max,currNode.hd+1);
+            if (currNode.node.right != null) {
+                q.add(new Info(currNode.node.right, currNode.hd + 1));
+                max = Math.max(max, currNode.hd + 1);
             }
         }
-        for (int i = min; i <=max; i++) {
+        for (int i = min; i <= max; i++) {
             System.out.print(map.get(i).data + " ");
         }
     }
+
     public ArrayList<Integer> ans = new ArrayList<>();
-    public void solveforRightView(Node root,int lvl)
-    {
-        if(root==null)
-        {
+
+    public void solveforRightView(Node root, int lvl) {
+        if (root == null) {
             return;
         }
-        if(lvl==ans.size())
-        {
+        if (lvl == ans.size()) {
             ans.add(root.data);
         }
 
-        solveforRightView(root.right,lvl+1);
-        solveforRightView(root.left,lvl+1);
+        solveforRightView(root.right, lvl + 1);
+        solveforRightView(root.left, lvl + 1);
     }
 
     public static Node lowestCommonAncestor(Node root, Node p, Node q) {
         ArrayList<Node> path1 = new ArrayList<>();
         ArrayList<Node> path2 = new ArrayList<>();
 
-        boolean path_1 = getPath(root,p,path1);
-        boolean path_2 = getPath(root,q,path2);
+        boolean path_1 = getPath(root, p, path1);
+        boolean path_2 = getPath(root, q, path2);
 
-        int i=0;
-        int j=0;
+        int i = 0;
+        int j = 0;
         Node ans = null;
-        while(i<path1.size() && j<path2.size())
-        {
-            if(path1.get(i).data!=path2.get(j).data)
-            {
+        while (i < path1.size() && j < path2.size()) {
+            if (path1.get(i).data != path2.get(j).data) {
                 break;
-            }
-            else
-            {
-               ans = path1.get(i);
+            } else {
+                ans = path1.get(i);
             }
             i++;
             j++;
@@ -167,67 +148,62 @@ public class buildtree {
         return ans;
     }
 
-    public static boolean getPath(Node root,Node x,ArrayList<Node> list)
-    {
-        if(root==null)
-        {
+    public static boolean getPath(Node root, Node x, ArrayList<Node> list) {
+        if (root == null) {
             return false;
         }
         list.add(root);
 
-        if(root.data==x.data)
-        {
+        if (root.data == x.data) {
             return true;
         }
 
-        boolean foundRight = getPath(root.right,x,list);
-        boolean foundLeft = getPath(root.left,x,list);
+        boolean foundRight = getPath(root.right, x, list);
+        boolean foundLeft = getPath(root.left, x, list);
 
-        if(foundRight || foundLeft)
-        {
+        if (foundRight || foundLeft) {
             return true;
         }
 
-        list.remove(list.size()-1);
+        list.remove(list.size() - 1);
         return false;
 
-
     }
+
     public static Node lowestCommonAncestor2(Node root, Node p, Node q) {
-        if(root==null || root.data==p.data || root.data==q.data)
-        {
+        if (root == null || root.data == p.data || root.data == q.data) {
             return root;
         }
 
-        Node rightLca = lowestCommonAncestor2(root.right,p,q);
-        Node leftLca = lowestCommonAncestor2(root.left,p,q);
+        Node rightLca = lowestCommonAncestor2(root.right, p, q);
+        Node leftLca = lowestCommonAncestor2(root.left, p, q);
 
-        if(rightLca==null)
-        {
+        if (rightLca == null) {
             return leftLca;
         }
-        if(leftLca==null)
-        {
+        if (leftLca == null) {
             return rightLca;
         }
         return root;
-        
+
     }
+
     public static void main(String[] args) {
         /*
-        *    1
-        *   / \
-        *  2   3
-        * /     \
-        *4       6
+         * 1
+         * / \
+         * 2 3
+         * / \ \
+         * 4 5 6
          */
-        int[]arr= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int[] arr = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
         Node root = buildTree(arr);
         // System.out.println(root.data);
         // preOrder(root);
         levelOrder(root);
         // topView(root);
-    //    Node ans =  lowestCommonAncestor(root, root.left.left, root.right.right);
-    //    System.out.println(ans.data);
+        // Node ans = lowestCommonAncestor(root, root.left.left, root.right.right);
+        // System.out.println(ans.data);
     }
 }
