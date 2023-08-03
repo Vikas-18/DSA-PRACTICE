66566a2a55d5de09eb1abcc51674f4a403c6d673
Vikas-18 <vikaskohli18impossible@gmail.com>
2023-08-03 09:38:47 +0530

    code changes

--- binarysearch/koko.java
+++ binarysearch/koko.java
@@ -1,47 +1,35 @@
 import java.util.*;
+
 public class koko {
     public static void main(String[] args) {
-        int[] pile = {3,6,7,11};
-       System.out.println(minEatingSpeed(pile,8));
-      
+        int[] pile = { 3, 6, 7, 11 };
+        System.out.println(minEatingSpeed(pile, 8));
+
     }
-    static int minEatingSpeed(int[] piles, int hour)
-    {
-         int max = Integer.MIN_VALUE;
-        for(int i=0; i<piles.length; i++)
-        {
-            max = Math.max(max,piles[i]);
+
+    public static int minEatingSpeed(int[] piles, int h) {
+        int s = 1;
+        int maxele = piles[0];
+        for (int i = 1; i < piles.length; i++) {
+            maxele = Math.max(maxele, piles[i]);
         }
-        if(hour==piles.length)
-        {
-            return max;
-        }
-        
-        int sp = Integer.MAX_VALUE;
-        int l=0;
-        int h = max;
-        
-        while(l<=h)
-        {
-            int speed = l+(h-l)/2;
-            if(isPossible(piles,speed,hour))
-            {
-                sp = speed;
-                h = speed-1;
-            }
-            else{
-                l = speed+1;
+        int e = maxele;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (solve(piles, mid) <= h) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
-        return sp;
+        return s;
     }
-   static boolean isPossible(int[]piles,int speed,int h)
-    {
-      int time=0;
-        for(int i=0; i<piles.length; i++)
-        {
-           time+= (int)Math.ceil(piles[i]*1.0/speed);
+
+    public static int solve(int[] arr, int h) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ans += Math.ceil((double) arr[i] / (double) h);
         }
-        return time<=h;
+        return ans;
     }
 }
