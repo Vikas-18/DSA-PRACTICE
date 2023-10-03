dfdf3392e2ca46b342c3dfeee809df3922651ac8
Vikas-18 <vikaskohli18impossible@gmail.com>
2023-10-03 08:33:20 +0530

    changes made

--- sorting/bubble_sort.java
+++ sorting/bubble_sort.java
@@ -3,15 +3,7 @@ public class bubble_sort {
     public static void main(String[] args) {
         // worst case: O(n*n) best case:O(n);
         int[] arr = { 0, 1, 3, 1, 3, -2 };
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1])// compare side by side & swap
-                {
-                    swap(j, j + 1, arr);
-                }
-
-            }
-        }
+        solve2(arr);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
@@ -24,4 +16,25 @@ public class bubble_sort {
         arr[y] = temp;
     }
 
+    public static void solve(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(j, j + 1, arr);
+                }
+            }
+        }
+    }
+
+    public static void solve2(int[] arr) {
+        for (int i = 0; i <= arr.length - 2; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(j, j - 1, arr);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
--- sorting/cyclic_sort.java
+++ sorting/cyclic_sort.java
@@ -4,34 +4,45 @@
 public class cyclic_sort {
 
     public static void main(String[] args) {
-        int [] arr = {1,2,3,5,7,6,8,4};
+        int[] arr = { 1, 2, 3, 5, 7, 6, 8, 4 };
 
-        cyclicsort(arr);
+        // cyclicsort(arr);
+        solve(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
-    static void cyclicsort(int[] arr)
-    {
-        int i=0;
-        
-        while(i<arr.length)
-        {
-            int correctindex = arr[i] -1;
-            if(arr[i]!=arr[correctindex])
-            {
-                swap(i,correctindex,arr);
-            }
-            else{
+
+    static void cyclicsort(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+            int correctindex = arr[i] - 1;
+            if (arr[i] != arr[correctindex]) {
+                swap(i, correctindex, arr);
+            } else {
                 i++;
             }
         }
     }
-    static void swap(int x,int y,int[] arr)
-    {
+
+    static void swap(int x, int y, int[] arr) {
         int temp = arr[x];
         arr[x] = arr[y];
         arr[y] = temp;
     }
-}
 
+    public static void solve(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+            int correctindex = arr[i] - 1;
+
+            if (arr[correctindex] != arr[i]) {
+                swap(i, correctindex, arr);
+            } else {
+                i++;
+            }
+        }
+    }
+}
