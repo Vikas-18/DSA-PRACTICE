e0d2961176554301cc808943fb99c4d3e91f50e3
Vikas-18 <vikaskohli18impossible@gmail.com>
2024-05-24 09:50:20 +0530

    solution updated

--- arrays/maxscorestring.java
+++ arrays/maxscorestring.java
@@ -7,37 +7,51 @@ public class maxscorestring {
     }
 
     public static int maxScoreWords(String[] words, char[] letters, int[] score) {
-        int ans = 0;
-
-        for (int i = 0; i < words.length; i++) {
-            String temp = "";
-            for (int j = 0; j < letters.length; j++) {
-                temp += letters[j];
-            }
-            String s = words[i];
-            if (check(s, temp)) {
-                int sum = 0;
-                for (int j = 0; j < s.length(); j++) {
-                    sum += score[s.charAt(j) - 'a'];
-                }
-                ans = Math.max(ans, sum);
-            }
+        int[] frq = new int[26];
+        for (int i = 0; i < letters.length; i++) {
+            frq[letters[i] - 'a']++;
         }
-        return ans;
+        return solve(words, 0, frq, score);
     }
 
-    public static boolean check(String s, String t) {
-        int[] arr = new int[26];
-        for (int i = 0; i < t.length(); i++) {
-            arr[t.charAt(i) - 'a']++;
+    public static int solve(String[] words, int idx, int[] frq, int[] score) {
+        if (idx == words.length) {
+            return 0;
         }
+
+        int notPick = solve(words, idx + 1, frq, score);
+        int pick = 0;
+        if (check(words[idx], frq)) {
+            for (int i = 0; i < words[idx].length(); i++) {
+                char ch = words[idx].charAt(i);
+                frq[ch - 'a']--;
+            }
+            pick = getScore(words[idx], score) + solve(words, idx + 1, frq, score);
+            for (int i = 0; i < words[idx].length(); i++) {
+                char ch = words[idx].charAt(i);
+                frq[ch - 'a']++;
+            }
+        }
+        return Math.max(pick, notPick);
+    }
+
+    public static boolean check(String s, int[] frq) {
+        int[] temp = frq.clone();
         for (int i = 0; i < s.length(); i++) {
-            if (arr[s.charAt(i) - 'a'] <= 0) {
+            if (temp[s.charAt(i) - 'a'] <= 0) {
                 return false;
             } else {
-                arr[s.charAt(i) - 'a']--;
+                temp[s.charAt(i) - 'a']--;
             }
         }
         return true;
+    }
+
+    public static int getScore(String s, int[] score) {
+        int sum = 0;
+        for (int j = 0; j < s.length(); j++) {
+            sum += score[s.charAt(j) - 'a'];
+        }
+        return sum;
     }
 }
